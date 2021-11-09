e4c438273cece2744fa1e69a21d6fb4bbf75856b
born3am <psmaranhao@gmail.com>
2021-11-09 22:23:01 +0100

    logo change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,14 @@ import React, { useContext } from 'react'
 import { Link } from "react-router-dom"
 import { MyContext } from '../App'
 import './Navbar.css'
+import logo from '../img/logo-ecommerce.png'
 
 export default function Navbar() {
     const {user} = useContext(MyContext)
     return (
-        <div>
+        <div className="nav-header" >
             <div className="logo">
-                <img width="50px" src="https://i.pinimg.com/originals/25/4d/86/254d865a465f88b330e6b45d82ef29e1.png" alt="" />
+                <img width="70px" src={logo} alt="" />
             </div>
 
             <nav>
