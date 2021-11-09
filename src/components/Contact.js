cf9cf50ef47632904cd75e513d1d76feeacf03d9
born3am <psmaranhao@gmail.com>
2021-11-09 15:32:23 +0100

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,23 @@ import React from 'react'
 export default function Contact() {
     return (
         <div>
-            <h1>Contact Page</h1>
+            <h1>Contact Us</h1>
+
+            <form action="">
+                <label>Name:
+                    <input type="text" name="name" /> <br />
+                </label>
+
+
+                <label >Email:
+                   <input type="email" name="email" /> <br />
+                </label>
+                
+                <textarea name="message" id="" cols="30" rows="10"></textarea>
+                <input type="submit" value="send" />
+
+            </form>
+
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,31 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React, { useContext } from 'react'
+import { Link } from "react-router-dom"
+import { MyContext } from '../App'
 
 export default function Navbar() {
+    const {user} = useContext(MyContext)
     return (
         <div>
             <div className="logo">
                 <img width="50px" src="https://i.pinimg.com/originals/25/4d/86/254d865a465f88b330e6b45d82ef29e1.png" alt="" />
             </div>
-        
-        <nav>
-            <ul>
-                <li> <Link to="/login" >Login</Link> </li>
-                <li> <Link to="/" >Home</Link> </li>
-                <li> <Link to="/products" >Products</Link> </li>
-                <li> <Link to="/cart" >Cart</Link> </li>
-                <li> <Link to="/profile" >Profile</Link> </li>
-                <li> <Link to="/contact" >Contact</Link> </li>
+
+            <nav>
+                <ul>
+                    {user ? <li> <Link to="/profile" >Profile</Link> </li> : <li> <Link to="/login" >Login</Link> </li>}
+
+                    <li> <Link to="/" >Home</Link> </li>
+                    <li> <Link to="/products" >Products</Link> </li>
+                    <li> <Link to="/cart" >Cart</Link> </li>
+
+                    <li> <Link to="/contact" >Contact</Link> </li>
 
 
 
 
 
-            </ul>
-        </nav>
+                </ul>
+            </nav>
 
         </div>
     )
