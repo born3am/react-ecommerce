716dc1a0d39a9e1028bab16682b89ecb1476a4d9
born3am <psmaranhao@gmail.com>
2021-11-10 11:52:01 +0100

    emailJS

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import EmailJS from 'emailjs-com'
 
 export default function Contact() {
+    const formRef = useRef()
+
+    const [status, setStatus] = useState(null)
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+        EmailJS.sendForm(
+            process.env.REACT_APP_SERVICE_ID,
+            process.env.REACT_APP_TEMPLATE_ID,
+            formRef.current,
+            process.env.REACT_APP_USER_ID)
+            .then((result) => {
+                console.log(result.text);
+                if (result.text === 'OK') {
+                    setStatus(true)
+                }
+            })
+            .catch(err => console.log(err));
+    }
+
     return (
         <div>
             <h1>Contact Us</h1>
+            {status ? <p>Thank you for your email</p> : <form ref={formRef} action="" onSubmit={sendEmail}>
+                <label htmlFor="name" >Name: </label>
+                <input type="text" name="user_name" id="name" /> <br />
 
-            <form action="">
-                <label>Name:
-                    <input type="text" name="name" /> <br />
-                </label>
 
 
                 <label >Email:
-                   <input type="email" name="email" /> <br />
+                    <input type="email" name="useremail" /> <br />
                 </label>
-                
-                <textarea name="message" id="" cols="30" rows="10"></textarea>
-                <input type="submit" value="send" />
 
-            </form>
+                <label htmlFor="message" >Message</label>
+                <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                <button>send email</button>
+            </form>}
+
+
 
         </div>
     )
