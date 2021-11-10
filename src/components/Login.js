814a7c6b339eded66f72c7be3a3ed7294c913f74
born3am <psmaranhao@gmail.com>
2021-11-10 15:26:03 +0100

    update

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup, GithubAuthProvider } from "firebase/auth"
+import GoogleButton from 'react-google-button'
+
+import { useContext } from "react";
+import { MyContext } from "../App";
+import { useNavigate } from 'react-router';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY, 
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth()
+const GoogleProvider = new GoogleAuthProvider()
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 
 export default function Login() {
-    return (
+
+    const { setUser } = useContext(MyContext)
+    const navigate = useNavigate()
+
+    const loginWithGoogle = () => {
+        signInWithPopup(auth, GoogleProvider)
+            .then(userInfo => {
+                setUser(userInfo.user)
+                navigate('/profile')
+            })
+            .catch(err => console.log(err.message))
+
+    }
+
+    // useEffect((() => {
+    //     let subscribe = onAuthStateChanged(auth)
+    //         .then(userInfo => {
+    //             setUser(userInfo.user)
+    //             navigate('/', { replace: true })
+    //         })
+    //         .catch(err => console.log(err.message))
+    // return subscribe
+
+    //     }, [])
+    
+
+
+return (
         <div>
-            <h1>Login Page</h1>
+            <h1>Google</h1>
+            <GoogleButton onClick={loginWithGoogle} />
+
+
+
         </div>
+
     )
 }
+
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
+import {getAuth, signOut} from 'firebase/auth'
+import { useNavigate} from 'react-router'
+
+import { useContext } from 'react'
+
+import { MyContext } from '../App'
+
+const auth = getAuth()
+
 export default function Profile() {
+
+    const navigate = useNavigate()
+    const {setUser, user} = useContext(MyContext)
+
+    const logOutGoogle=()=>{
+        signOut(auth).then(()=>{
+            setUser(null)
+            navigate("/" , {replace:true})
+        })
+        .catch(err=>{console.log(err.message)});
+    }
+
     return (
         <div>
-            Profile Page
+            <h1>{user.displayName}</h1>
+            <img src={user.photoURL} alt="" />
+
+            <button onClick={logOutGoogle}>logout</button>
         </div>
     )
 }
